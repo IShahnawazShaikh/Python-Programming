01b44a7b250eebb4acf4d65846998ce35ce96ed2
shahnawazshaikh123 <32239343+shahnawazshaikh123@users.noreply.github.com>
2020-04-19 08:12:36 +0530

    Format Method
    Getting started with Python with simple code.

---
+++ area_circle.py
@@ -0,0 +1,3 @@
+radius=input('enter radius ')
+area=3.14*int(radius)**2
+print('area of circle for radius ',radius,'is= ',area)
---
+++ place_holder.py
@@ -0,0 +1,7 @@
+num1=1.65646
+num2=20.6545354
+print('first Number is = {0:.3} Second Number is ={1:.3F}'.format(num1,num2))
+
+# In more Simple Way
+
+print(f'First Number is = {num1:.3} , second number is = {num2:.3f}')
